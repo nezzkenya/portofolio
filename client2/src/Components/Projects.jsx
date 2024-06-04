78fa4981f63ab0e6c9f043f2aba2f434ad6a831c
nezz <72879112+nezzkenya@users.noreply.github.com>
2024-06-04 14:31:19 +0000

    page

--- client2/src/Components/Projects.jsx
+++ client2/src/Components/Projects.jsx
@@ -1,52 +1,19 @@
 import React from "react";
-
+import {NavLink} from "react-router-dom"
 export const Projects = () => {
   return (
     <div className=" bg-emerald-200 rounded-md p-3 items-center justify-center">
       <div className=" grid items-center justify-center">
         <div className=" justify-center p-2 pr-10 ">
-          <h4 className=" text-center font-bold uppercase">project sample</h4>
-        </div>
-        <div className="grid mx:flex">
-          <div className="hidden mx:grid ring-1 rounded-lg shadow-xl shadow-black drop-shadow-sm w-fit mx-5 h-full">
-            <div className=" flex h-50 ">
-              <div className=" grid w-fit gap-3 justify-center items-center">
-                <h4 className=" text-center ">Desktop view</h4>
-                <img
-                  className=" rounded-md p-1"
-                  src="https://i.ibb.co/BsbJ1bH/Screenshot-2024-05-21-121923.png"
-                  alt="Screenshot-2024-05-21-121923"
-                  border="0"
-                ></img>
-                <img
-                  className=" rounded-md p-1"
-                  src="https://i.ibb.co/GRK2kyw/Screenshot-2024-05-21-125014.png"
-                  alt="Screenshot-2024-05-21-121923"
-                  border="0"
-                ></img>
-              </div>
-            </div>
-          </div>
-          <div>
-            <div className=" ring-1 rounded-lg shadow-xl shadow-black drop-shadow-sm w-fit mx-5 h-full">
-              <h4 className=" text-center">Mobile view</h4>
-              <img
-                className="p-2 rounded-lg"
-                width={290}
-                src="https://i.ibb.co/Sy2cWBf/photo-5938105858443559524-y.jpg"
-                alt="photo-5938105858443559524-y"
-                border="0"
-              />
-            </div>
+          <h4 className=" text-center font-bold uppercase">projects</h4>
+          <div className=" grid items-center justify-center">
+<div className=" ring-1 rounded-md ">
+  <img src="../assets/Screenshot 2024-05-21 121923.png" className=" rounded-sm"/>
+  <h1 className=" font-semibold dark:text-slate-50">Mixtube</h1>
+  <NavLink to={"https://kenyanmix.vercel.app/"} className=" rounded-sm ring-1 ring-slate-50 p-1 dark:text-slate-50">Open Demo</NavLink>
+</div>
           </div>
         </div>
-      </div>
-      <div className="flex p-2  pt-8 justify-center">
-        <a href="https://kenyanmix.vercel.app/" target="_blank">
-          <button className=" bg-orange-600 p-2 rounded-lg text-slate-50">
-            View web app
-          </button>
-        </a>
       </div>
     </div>
   );
